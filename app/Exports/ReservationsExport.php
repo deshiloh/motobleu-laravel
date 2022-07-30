529bcd9807fcdad531628a57fed2518e3fb2dbaf
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2022-07-30 14:21:23 +0200

    wip

--- app/Exports/ReservationsExport.php
+++ app/Exports/ReservationsExport.php
@@ -8,22 +8,34 @@ use Illuminate\View\View;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
 use Maatwebsite\Excel\Concerns\WithDefaultStyles;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Events\AfterSheet;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
 use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Style\Style;
+use PhpOffice\PhpSpreadsheet\Worksheet\BaseDrawing;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use PhpOffice\PhpSpreadsheet\Style\Color;
 
-class ReservationsExport implements FromView, WithStyles, ShouldAutoSize, WithDefaultStyles
+class ReservationsExport implements WithStyles, ShouldAutoSize, WithDefaultStyles, WithCustomStartCell, WithHeadings, WithEvents, FromCollection, WithDrawings
 {
-    public function view(): View
+    private \Illuminate\Database\Eloquent\Collection $reservations;
+
+    public function __construct()
     {
-        return view('exports.reservations', [
-            'reservations' => Reservation::all()
-        ]);
+        $this->reservations = $this->getReservations();
+    }
+
+    public function collection()
+    {
+        return $this->reservations;
     }
 
     public function defaultStyles(Style $defaultStyle)
@@ -31,16 +43,27 @@ class ReservationsExport implements FromView, WithStyles, ShouldAutoSize, WithDe
         return [
             'alignment' => [
                 'horizontal' => Alignment::HORIZONTAL_CENTER,
+                'vertical' => Alignment::VERTICAL_CENTER,
             ],
         ];
     }
 
     public function styles(Worksheet $sheet)
     {
-        return [
-            '1' => [
+
+        $styles = [
+            'A1:E1' => [
                 'fill' => [
-                    'fillType'   => Fill::FILL_SOLID,
+                    'fillType' => Fill::FILL_SOLID,
+                    'startColor' => ['argb' => 'FF09158D'],
+                ],
+                'alignment' => [
+                    'indent' => 1,
+                ],
+            ],
+            'A2:E2' => [
+                'fill' => [
+                    'fillType' => Fill::FILL_SOLID,
                     'startColor' => ['argb' => 'FF09158D'],
                 ],
                 'font' => [
@@ -56,7 +79,65 @@ class ReservationsExport implements FromView, WithStyles, ShouldAutoSize, WithDe
                         'color' => ['argb' => Color::COLOR_BLACK]
                     ]
                 ]
-            ]
+            ],
         ];
+
+        for ($i = 3; $i <= $this->reservations->count() + 2; $i++) {
+            if ($i % 2 == 0) {
+                $styles[$i] = [
+                    'fill' => [
+                        'fillType' => Fill::FILL_SOLID,
+                        'startColor' => ['argb' => 'FFE0E0E0'],
+                    ],
+                ];
+            }
+        }
+        return $styles;
+    }
+
+    public function startCell(): string
+    {
+        return 'A7';
+    }
+
+    private function getReservations()
+    {
+        return Reservation::all('reference', 'pickup_date');
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Référence',
+            'Date'
+        ];
+    }
+
+    /*public function view(): \Illuminate\Contracts\View\View
+    {
+        return view('exports.reservations', [
+            'reservations' => $this->reservations
+        ]);
+    }*/
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function(AfterSheet $sheet) {
+                $sheet->sheet->getDelegate()->getRowDimension(1)->setRowHeight(80);
+            }
+        ];
+    }
+
+    public function drawings()
+    {
+        $drawing = new Drawing();
+        $drawing->setName('Logo');
+        $drawing->setDescription('This is my logo');
+        $drawing->setPath(public_path('storage/Motobleu.png'));
+        $drawing->setHeight(90);
+        $drawing->setCoordinates('A1');
+
+        return $drawing;
     }
 }
--- resources/views/exports/reservations.blade.php
+++ resources/views/exports/reservations.blade.php
@@ -1,16 +1,21 @@
 <table>
     <thead>
-    <tr>
-        <th>Référence</th>
-        <th>Date</th>
-    </tr>
+        <tr>
+            <th>Référence</th>
+            <th>Date</th>
+        </tr>
     </thead>
     <tbody>
     @foreach($reservations as $reservation)
-        <tr style="background-color: red;">
+        <tr>
             <td>{{ $reservation->reference }}</td>
             <td>{{ $reservation->pickup_date->format('d/m/Y') }}</td>
         </tr>
     @endforeach
+        <tr>
+            <td colspan="2" style="text-align: right;">
+                <img src="{{ public_path('storage/logo-pdf.png') }}" alt="" height="20">
+            </td>
+        </tr>
     </tbody>
 </table>
