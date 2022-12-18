ffbb342a7239f7587c8f77f22354ca02848245f6
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2022-12-18 12:06:07 +0100

    fix(Artisan): Suppression de la commande Meilisearch

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -58,48 +58,5 @@ class DatabaseSeeder extends Seeder
         Localisation::factory()->count(30)->create();
 
         Pilote::factory()->count(20)->create();
-
-        /*
-        $entreprises = Entreprise::factory()
-            ->count(10)
-            ->has(AdresseEntreprise::factory()->facturation())
-            ->has(AdresseEntreprise::factory()->physique())
-            ->create();
-
-        foreach ($entreprises as $entreprise) {
-            $users = User::factory()
-                ->count(4)
-                ->has(AdresseReservation::factory())
-                ->create();
-
-            foreach ($users as $user) {
-                $entreprise->has($user);
-
-                $passager = Passager::factory()->for($user)->create();
-                if (App::environment(['testing'])) {
-                    $facture = Facture::factory()->create();
-                    Reservation::factory([
-                        'pickup_date' => Carbon::now()
-                    ])
-                        ->for($passager)
-                        //->for($facture)
-                        ->create();
-                }
-                Reservation::factory([
-                    'is_confirmed' => true
-                ])->for($passager)->create();
-            }
-        }
-
-        if (App::environment(['local', 'prod'])) {
-            Localisation::factory()->count(30)->create();
-        }
-
-        Pilote::factory()->count(20)->create();
-
-        if (App::environment(['local', 'prod'])) {
-            Artisan::call('melisearch:setting');
-        }
-        */
     }
 }
