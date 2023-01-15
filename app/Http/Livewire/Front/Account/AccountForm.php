cf621aeb96c6c1474bdf360b9a4cfbb99a3cf538
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-01-15 15:43:22 +0100

    feat(Front): Ajout de la partie gestion assistantes (#55)

---
+++ app/Http/Livewire/Front/Account/AccountForm.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Livewire\Front\Account;
+
+use App\Models\Entreprise;
+use App\Models\User;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Hash;
+use Livewire\Component;
+use WireUi\Traits\Actions;
+
+class AccountForm extends Component
+{
+    use Actions;
+
+    public User $user;
+    public int $userId;
+    public bool $contextNewUser;
+
+    public function mount(User $account)
+    {
+        $this->user = $account;
+        $this->userId = \Auth::user()->id;
+
+        $this->contextNewUser = !$account->exists;
+
+        if ($this->contextNewUser) {
+            $this->user->is_actif = false;
+            $this->user->is_admin_ardian = false;
+        }
+    }
+
+    protected function getRules(): array
+    {
+        $rules = [
+            'user.nom' => 'required',
+            'user.prenom' => 'required',
+            'user.telephone' => 'required',
+            'user.adresse' => 'required',
+            'user.adresse_bis' => 'nullable',
+            'user.code_postal' => 'required',
+            'user.ville' => 'required',
+            'user.is_admin_ardian' => 'boolean',
+            'user.is_actif' => 'boolean',
+        ];
+
+        if ($this->user->exists) {
+            $rules['user.email'] = 'required|email';
+        } else {
+            $rules['user.email'] = 'required|email|unique:users,email';
+        }
+
+        return $rules;
+    }
+
+    public function render()
+    {
+        return view('livewire.front.account.account-form')
+            ->layout('components.front-layout');
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        try {
+            if ($this->contextNewUser) {
+                $this->user->password = Hash::make('test');
+                $this->user->save();
+            }
+
+            $this->user->entreprises()->attach(Entreprise::find(\Auth::user()->entreprises()->first()->id));
+
+            $this->user->update();
+
+            $this->notification()->success(
+                title : $this->user->exists ? 'Compte modifié' : 'Compte créé',
+                description : $this->user->exists ? 'Le compte a bien été modifié' : 'Le compte a bien été créé'
+            );
+
+            if ($this->contextNewUser) $this->resetFields();
+
+        } catch (\Exception $exception) {
+            if (App::environment(['local'])) {
+                ray([
+                    'user' => $this->user
+                ])->exception($exception);
+            }
+            // TODO Sentry en production
+        }
+    }
+
+    private function resetFields()
+    {
+        $this->user->nom = null;
+        $this->user->prenom = null;
+        $this->user->email = null;
+        $this->user->adresse = null;
+        $this->user->adresse_bis = null;
+        $this->user->code_postal = null;
+        $this->user->ville = null;
+    }
+}
---
+++ resources/views/livewire/front/account/account-data-table.blade.php
@@ -0,0 +1,53 @@
+<div>
+    <x-front.card>
+        <x-front.title>
+            {{ __('Liste des secrétaires') }}
+            <x-slot:button>
+                <x-button primary icon="plus" label="{{ __('Ajouter une secrétaire') }}" href="{{ route('front.user.create') }}"/>
+            </x-slot:button>
+        </x-front.title>
+        <x-datatable>
+            <x-slot:headers>
+                <x-datatable.tr>
+                    <x-datatable.th>{{ __('Nom') }}</x-datatable.th>
+                    <x-datatable.th>{{ __('Prénom') }}</x-datatable.th>
+                    <x-datatable.th>{{ __('Actif') }}</x-datatable.th>
+                    <x-datatable.th>Actions</x-datatable.th>
+                </x-datatable.tr>
+            </x-slot:headers>
+            <x-slot:body>
+                @forelse($users as $user)
+                    <x-datatable.tr>
+                        <x-datatable.td>{{ $user->nom }}</x-datatable.td>
+                        <x-datatable.td>{{ $user->prenom }}</x-datatable.td>
+                        <x-datatable.td>
+                            @if($user->is_actif)
+                                <x-front.badge success>
+                                    {{ __('Oui') }}
+                                </x-front.badge>
+                                @else
+                                <x-front.badge danger>
+                                    {{ __('Non') }}
+                                </x-front.badge>
+                            @endif
+                        </x-datatable.td>
+                        <x-datatable.td>
+                            <div class="space-x-2">
+                                <x-button.circle primary icon="pencil" href="{{ route('front.user.edit', ['account' => $user->id]) }}"/>
+                                <x-button.circle red icon="trash" />
+                            </div>
+                        </x-datatable.td>
+                    </x-datatable.tr>
+                    @empty
+                    <x-datatable.tr>
+                        <x-datatable.td colspan="3">
+                            <div class="text-center">
+                                {{ __('Aucune assistante') }}
+                            </div>
+                        </x-datatable.td>
+                    </x-datatable.tr>
+                @endforelse
+            </x-slot:body>
+        </x-datatable>
+    </x-front.card>
+</div>
