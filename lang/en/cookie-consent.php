5010cbf46aa19625e5642bce28acbe0a3c3dcccb
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-01-20 16:54:47 +0100

    wip

---
+++ lang/en/cookie-consent.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'texts' => [
+        'message' => 'We use cookies to ensure the best experience on our site. If you continue without changing your settings, we will assume that you consent to their use.',
+        'agree' => 'Continue'
+    ]
+];
---
+++ lang/fr/cookie-consent.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'texts' => [
+        'message' => 'Nous utilisons des cookies pour vous assurer la meilleure expérience sur notre site. Si vous continuez sans changer vos paramètres, nous supposons que vous consentez à les utiliser.',
+        'agree' => 'Continuer'
+    ]
+];
