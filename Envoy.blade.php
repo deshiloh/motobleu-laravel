f64b2fd4b3ea734e7d5d32e07c270bb0a0bb9a08
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-01-27 21:04:26 +0100

    wip

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,12 +1,19 @@
-@include('vendor/autoload.php')
 @servers(['web' => 'pascalgavalda@172.16.78.131'])
 
-@story('deploy')
-    get-project
-@endstory
-
-@task('get-project')
-    cd /var/www/vhosts/motobleu-paris.com/test.motobleu-paris.com/www
+@task('prod', ['confirm' => true])
+    cd /var/www/vhosts/motobleu-paris.com/
     git checkout master
     git pull
+
+    composer install
+    php artisan migrate
+@endtask
+
+@task('beta', ['confirm' => true])
+    cd /var/www/vhosts/motobleu-paris.com/test.motobleu-paris.com/www
+    git checkout develop
+    git pull
+
+    composer install
+    php artisan migrate:fresh --seed
 @endtask
