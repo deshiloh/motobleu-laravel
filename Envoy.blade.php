4a4df036cc2b7dd34cacc1c2c5ba1ccff4b5f250
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-01-28 12:52:23 +0100

    wip

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -5,8 +5,11 @@
     git checkout master
     git pull
 
-    composer install
+    composer install --optimize-autoloader --no-dev
     php artisan migrate
+    php artisan config:cache
+    php artisan route:cache
+    php artisan view:cache
 @endtask
 
 @task('beta', ['confirm' => true])
@@ -14,6 +17,9 @@
     git checkout develop
     git pull
 
-    composer install
-    php artisan migrate:fresh --seed
+    /opt/plesk/php/8.1/bin/php /usr/lib/plesk-9.0/composer.phar install --optimize-autoloader
+    /opt/plesk/php/8.1/bin/php artisan migrate:fresh --seed --force
+    /opt/plesk/php/8.1/bin/php artisan route:cache
+    /opt/plesk/php/8.1/bin/php artisan view:cache
+    npm install && npm run build
 @endtask
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-    <img src="{{ url('storage/Motobleu.png') }}" class="logo" alt="motobleu Logo">
+    <img src="{{ url('storage/motobleu.png') }}" class="logo" alt="motobleu Logo">
 </a>
 </td>
 </tr>
