6340e1d77532eb687686030a5e7036d6ad2dbe04
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-01-28 14:40:17 +0100

    feat(Application): Script déploiement pour la prod

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -2,14 +2,27 @@
 
 @task('prod', ['confirm' => true])
     cd /var/www/vhosts/motobleu-paris.com/
-    git checkout master
-    git pull
+    mkdir temp
 
-    composer install --optimize-autoloader --no-dev
-    php artisan migrate
-    php artisan config:cache
-    php artisan route:cache
-    php artisan view:cache
+    cd /var/www/vhosts/motobleu-paris.com/temp
+    git clone -b develop https://github.com/deshiloh/motobleu-laravel ./
+
+    cp ../.env-beta ./.env
+    /opt/plesk/php/8.1/bin/php /usr/lib/plesk-9.0/composer.phar install --optimize-autoloader --no-dev
+    npm install && npm run build
+    /opt/plesk/php/8.1/bin/php artisan key:generate
+    /opt/plesk/php/8.1/bin/php artisan migrate --force
+    /opt/plesk/php/8.1/bin/php artisan route:cache
+    /opt/plesk/php/8.1/bin/php artisan view:cache
+
+    cd /var/www/vhosts/motobleu-paris.com/
+    cp -R ./www/storage/app/photos ./photos
+    rm -rf www/
+    mv ./temp/ ./www/
+    mv ./photos ./www/storage/app/photos
+
+    cd /var/www/vhosts/motobleu-paris.com/www
+    /opt/plesk/php/8.1/bin/php artisan storage:link
 @endtask
 
 @task('beta', ['confirm' => true])
