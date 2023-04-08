e6eaedf6dd445ffee3443a61f49cec5486b3bde4
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-04-08 11:46:21 +0200

    fix(Menu): Les liens des menus sont maintenant plus clair par défaut

--- resources/views/components/front/menu/item.blade.php
+++ resources/views/components/front/menu/item.blade.php
@@ -5,7 +5,7 @@
 <a {{ $attributes->except(['class']) }}
    @class([
         'block max-sm:py-3 sm:inline-flex items-center sm:border-b-2 px-1 pt-1 text-sm font-medium flex space-x-2 transition transition-all',
-        'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-300' => !$active,
+        'border-transparent text-gray-400 hover:border-gray-200 hover:text-gray-200' => !$active,
         'max-sm:bg-white max-sm:text-motobleu border-white-500 text-white' => $active
     ])
 >
