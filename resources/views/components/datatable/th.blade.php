6fc282ec0ac0bf48b69022bec3b64ba359694c22
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-04-10 19:56:51 +0200

    fix(Tableau): Optimisations

--- resources/views/components/datatable/th.blade.php
+++ resources/views/components/datatable/th.blade.php
@@ -4,7 +4,7 @@
     'active' => false
 ])
 
-<th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-100 sm:pl-6" {{ $attributes }}>
+<th scope="col" {{ $attributes->merge(['class' => 'py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 dark:text-gray-100 sm:pl-6']) }}>
     @unless($sortable)
         {{ $slot }}
         @else
