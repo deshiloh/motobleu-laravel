5e85582ad9fa119c54f913b176c66a73046f7c64
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-05-10 19:42:32 +0200

    wip

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,11 @@ use App\Models\Pilote;
 use App\Models\TypeFacturation;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +25,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required'
+    ]);
 
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    $user->tokens()->where('name', $request->device_name)->delete();
+
+    return new JsonResponse([
+        'token' => $user->createToken($request->device_name)->plainTextToken
+    ]);
+});
+
+Route::get("/test", function () {
+    return new JsonResponse([
+        'response' => 'test'
+    ]);
+})->middleware(['auth:sanctum']);
