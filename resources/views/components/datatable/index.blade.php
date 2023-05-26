1141a7f288b2bfdd70fc2e5cc86c7dedda00d7ab
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-05-26 19:42:26 +0200

    fix(Tableaux): Changement d'opacité quand la page charge

--- resources/views/components/datatable/index.blade.php
+++ resources/views/components/datatable/index.blade.php
@@ -2,7 +2,7 @@
     'headers' => false,
     'footer' => false
 ])
-<div class="flex flex-col">
+<div class="flex flex-col" wire:loading.class="opacity-25">
     <div class="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
         <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
             <div class="shadow ring-1 ring-black dark:ring-slate-900 ring-opacity-5 md:rounded-lg overflow-hidden">
