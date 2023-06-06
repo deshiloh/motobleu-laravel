edc0e7a9b430e7499e3c9a239401846a96dc26e0
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-06-06 22:57:12 +0200

    fix(Pilote): Modification d'une course se répercute sur l'évènement Google Calendar

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -54,7 +54,7 @@ class EventServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         User::observe(AccountObserver::class);
         Reservation::observe(ReservationObserver::class);
@@ -65,7 +65,7 @@ class EventServiceProvider extends ServiceProvider
      *
      * @return bool
      */
-    public function shouldDiscoverEvents()
+    public function shouldDiscoverEvents(): bool
     {
         return false;
     }
