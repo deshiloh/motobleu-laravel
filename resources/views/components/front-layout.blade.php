7cc082cb5054fc1fc2b3716bb636f485fddb7e52
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-08-19 12:13:41 +0200

    fix(Page): Ajout du lien pour aller sur l'accueil du site

--- resources/views/components/front-layout.blade.php
+++ resources/views/components/front-layout.blade.php
@@ -45,6 +45,15 @@
                 <div class="flex h-16 justify-between">
                     <div class="flex">
                         <div class="hidden sm:ml-6 sm:flex sm:space-x-4 2xl:space-x-8">
+                            @guest
+                                <x-front.menu.item href="{{ route('front.home') }}">
+                                    <x-slot:icon>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                                        </svg>
+                                    </x-slot:icon>
+                                </x-front.menu.item>
+                            @endguest
                             <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
                             <x-menu.front />
                         </div>
