32551037bb1261dc86098bed7186452e2fb8c68f
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2023-08-20 11:59:44 +0200

    feat(Adresse Entreprise): Possibilité de supprimer une adresse entreprise

--- app/Http/Livewire/Entreprise/AdressesEntrepriseDataTable.php
+++ app/Http/Livewire/Entreprise/AdressesEntrepriseDataTable.php
@@ -9,9 +9,12 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 use Livewire\WithPagination;
+use WireUi\Traits\Actions;
 
 class AdressesEntrepriseDataTable extends Component
 {
+    use Actions;
+
     public Entreprise $entreprise;
 
     /**
@@ -22,5 +25,33 @@ class AdressesEntrepriseDataTable extends Component
         return view('livewire.entreprise.adresses-entreprise-data-table', [
             'adresses' => AdresseEntreprise::where('entreprise_id', $this->entreprise->id)->get()
         ]);
+    }
+
+    public function deleteAddress(AdresseEntreprise $adresseEntreprise): void
+    {
+        try {
+            $adresseEntreprise->delete();
+
+            $this->notification()->success(
+                title: "Opération réussite !",
+                description: "L'adresse a bien été supprimée."
+            );
+        } catch (\Exception $exception) {
+            $this->notification()->error(
+                title: "Une erreur est survenue",
+                description: "Une erreur est survenue pendant la suppression de l'adresse"
+            );
+
+            if (\App::environment(['local'])) {
+                ray()->exception($exception);
+            }
+
+            if (\App::environment(['prod', 'beta'])) {
+                \Log::channel("sentry")->error("Erreur pendant la suppression de l'adresse", [
+                    'exception' => $exception,
+                    'user' => $adresseEntreprise
+                ]);
+            }
+        }
     }
 }
