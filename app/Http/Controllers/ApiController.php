3c3fa2904e6b516cbac07c802f2d38753ff81d7d
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2024-04-19 23:39:09 +0200

    wip

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enum\ReservationStatus;
 use App\Events\ReservationConfirmed;
 use App\Http\Resources\ReservationResource;
 use App\Mail\PiloteAttached;
@@ -11,9 +12,71 @@ use App\Models\Reservation;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 
 class ApiController extends Controller
 {
+    /**
+     * @param Request $request
+     * @param Reservation $reservation
+     * @return JsonResponse
+     */
+    public function confirmationAction(Request $request, Reservation $reservation): JsonResponse
+    {
+        // Vérification que le pilote est bien renseigné dans la request.
+        if (!$request->has('pilote')) {
+            return new JsonResponse([
+                "message" => "Le pilote n'est pas renseigné."
+            ], 400);
+        }
+
+        if (!$request->has('message')) {
+            return new JsonResponse([
+                'message' => 'Le message de l\'email est manquant'
+            ], 400);
+        }
+
+        // On vérifie que le pilote existe bien dans la base de données
+        try {
+            $pilote = Pilote::findOrFail($request->post('pilote'));
+        } catch (Exception) {
+            return new JsonResponse([
+                "message" => "Pilote non trouvé"
+            ], 404);
+        }
+
+        $reservation->statut = ReservationStatus::Confirmed->value;
+        $reservation->pilote_id = $pilote->id;
+        $reservation->encaisse_pilote = $request->post('encaisse');
+        $reservation->encompte_pilote = $request->post('encompte');
+        $reservation->comment_pilote = $request->post('commentPilote');
+
+        $reservation->update();
+
+        try {
+            \Mail::to($reservation->pilote->email)
+                ->send(new PiloteAttached($reservation));
+        } catch (Exception $exception) {
+            if (\App::environment(['local'])) {
+                ray()->exception($exception);
+            }
+
+            if (App::environment(['beta', 'prod'])) {
+                Log::channel('sentry')->error("Erreur pendant la génération Google Calendar", [
+                    'exception' => $exception,
+                    'reservation' => $reservation
+                ]);
+            }
+        }
+
+        ReservationConfirmed::dispatch($reservation, $request->post('message'));
+
+        return new JsonResponse([
+            'message' => "réservation confirmée"
+        ], 200);
+    }
+
     public function handleAction(Request $request, Reservation $reservation, string $action)
     {
         if ($request->has('pilote')) {
@@ -72,7 +135,7 @@ class ApiController extends Controller
 
     private function handleConfirmation(Reservation $reservation, string $message)
     {
-        $reservation->statut == \App\Enum\ReservationStatus::Confirmed;
+        $reservation->statut = \App\Enum\ReservationStatus::Confirmed;
         $reservation->refresh();
 
         \Mail::to($reservation->pilote->email)
