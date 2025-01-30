ea6ae0a0a1b2a26dae2b1601cfa963caee2ec785
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2025-01-30 11:19:14 +0100

    wip change toggle button

--- resources/views/livewire/facturation/facturation-data-table.blade.php
+++ resources/views/livewire/facturation/facturation-data-table.blade.php
@@ -67,13 +67,17 @@
                         <x-datatable.td>{{ $facture->created_at->format('d/m/Y') }}</x-datatable.td>
                         <x-datatable.td>
                             <div class="flex space-x-2">
-                                @if($facture->statut !== \App\Enum\BillStatut::CREATED)
-                                    <x-toggle x-on:click="$wire.toggleAcquitte({{$facture}})" :checked="$facture->is_acquitte" md/>
-                                @endif
                                 @if($facture->is_acquitte)
                                     <span class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-green-100 text-green-800"> Oui </span>
                                 @else
                                     <span class="inline-flex items-center px-2 py-0.5 rounded text-xs font-medium bg-red-100 text-red-800"> Non </span>
+                                @endif
+                                @if($facture->statut !== \App\Enum\BillStatut::CREATED)
+                                    <button wire:click="toggleAcquitte({{$facture}})" type="button" class="rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                                        </svg>
+                                    </button>
                                 @endif
                             </div>
                         </x-datatable.td>
