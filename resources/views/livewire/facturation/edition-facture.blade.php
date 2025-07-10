a1c61ddb3709fa3ce8f0fb1612cdbd64d41d7de6
Manuel Alvarez-Iglisias <m.alvarez.iglisias@gmail.com>
2025-07-10 20:50:10 +0200

    feat(Facture): possiblité de modifier le champ adresse client et adresse facturation.

--- resources/views/livewire/facturation/edition-facture.blade.php
+++ resources/views/livewire/facturation/edition-facture.blade.php
@@ -259,6 +259,8 @@
                         <x-tinymce wire:model="email.message"/>
                         <x-toggle wire:model.defer="facture.is_acquitte" label="Facture acquittée"/>
                         <x-tinymce wire:model.defer="facture.information" label="Informations"/>
+                        <x-tinymce wire:model.defer="facture.adresse_client" label="Adresse client"/>
+                        <x-tinymce wire:model.defer="facture.adresse_facturation" label="Adresse Facturation"/>
 {{--                        <x-textarea label="Texte information" hint="Ce texte apparaitra sur la facture" wire:model.defer="email.complement" wire:change.debounce="editFactureAction"/>--}}
                         <x-button wire:click="sendEmailTestAction" primary sm type="button" icon="mail">Envoi d'un email de test</x-button>
                         <x-button wire:click="exportAction" info sm type="button" icon="download">Récap. des courses</x-button>
